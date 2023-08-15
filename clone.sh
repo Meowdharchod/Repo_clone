ed17121066800701c3c37ce37504fc5ec924301a
Meowdharchod <107068268+Meowdharchod@users.noreply.github.com>
2023-08-15 09:53:48 +0530

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/meowdharchod1/android_vendor_realme_sm8250-common vendor/realme/sm8250-common
+git clone https://github.com/meowdharchod/android_vendor_realme_bitra vendor/realme/bitra
+git clone https://github.com/meowdharchod/android_device_realme_sm8250-common device/realme/sm8250-common
+git clone https://github.com/meowdharchod/android_device_realme_bitra device/realme/bitra
+git clone https://github.com/meowdharchod/kernel_realme_sm8250  kernel/realme/sm8250
+git clone https://github.com/meowdharchod/android_hardware_oplus hardware/oplus
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
