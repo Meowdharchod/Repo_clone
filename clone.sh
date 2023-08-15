e995bd12ce64b9d7f77f0c2eec6fd3291bdee984
Meowdharchod <107068268+Meowdharchod@users.noreply.github.com>
2023-08-15 10:04:37 +0530

    Update clone.sh
    Typo

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/meowdharchod1/android_vendor_realme_sm8250-common vendor/realme/sm8250-common
+git clone https://github.com/meowdharchod/android_vendor_realme_sm8250-common vendor/realme/sm8250-common
 git clone https://github.com/meowdharchod/android_vendor_realme_bitra vendor/realme/bitra
 git clone https://github.com/meowdharchod/android_device_realme_sm8250-common device/realme/sm8250-common
 git clone https://github.com/meowdharchod/android_device_realme_bitra device/realme/bitra
