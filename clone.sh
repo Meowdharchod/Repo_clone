973a8a3ed44d2a00fccafec64d46a50aec6e15b0
Meowdharchod <107068268+Meowdharchod@users.noreply.github.com>
2023-08-29 00:13:29 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,4 +4,3 @@ git clone https://github.com/meowdharchod/android_device_realme_sm8250-common de
 git clone https://github.com/meowdharchod/android_device_realme_bitra device/realme/bitra
 git clone https://github.com/meowdharchod/kernel_realme_sm8250  kernel/realme/sm8250
 git clone https://github.com/meowdharchod/android_hardware_oplus hardware/oplus
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
